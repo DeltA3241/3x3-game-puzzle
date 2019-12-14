f200dfc7fbc9ce48c00c6cebe1e1d3adc1723006
DeltA3241 <55913416+DeltA3241@users.noreply.github.com>
2019-12-14 21:00:14 +0500

    Add files via upload
    Multiple changes:
    -the state diagram of all possible moves that can be made from the inital point to the last point has been added.
    -the code gives out all possible values till it finds the last state 
    -Backtracking back and giving the solution hasnt been added in this version

--- 8-puzzle.cpp
+++ 8-puzzle.cpp
@@ -6,12 +6,13 @@ const int value = 3;
 class puzzle8{
     struct node {
         queue<int>puzzle;
-        node* next;
+        node* parent;
+        node* children[];
         int array[value][value];
     };
-    node* parent = new node;
     int array2[value][value];
-
+    int x1;
+    int y1;
 public:
 
 
@@ -38,16 +39,77 @@ public:
                 }
             }
         }
-        bool checkpuzzle() {
-
+        bool checkpuzzle(node* temp1) {
             for (int x = 0;x < 3;++x) {
                 for (int y = 0;y < 3;++y) {
-                    if (temp.array[x][y] != array2[x][y]) {
+                    if (temp1.array[x][y] != array2[x][y]) {
                         return false;
                    }
                 }
             }
             return true;
+        }
+
+        void getblank() {
+          
+            for (int x = 0;x < 3;++x) {
+                for (int z = 0;z < 3;++z) {
+                    if (array2[x][z] == 0) {
+                        x1 = x;
+                        y1 = z;
+                    }
+                }
+            }
+        }
+
+        int movexpansion(node* temp1) {
+            int u;
+            //up
+            if (temp1.puzzle.front() != temp1.array[x1-1][y1] ){
+                if (x1 == 0) {
+                    return 0;
+                }
+                else {
+                    node* temp2 = new node;
+                    
+
+             
+                }
+
+        }
+            //down
+         
+            if (x1 == 2) {
+                return 0;
+            }
+            else {
+                u = temp1.array[x1][y1];
+                temp1.array[x1][y1] = temp1.array[x1+1][y1];
+                temp1.array[x1+1][y1] = u;
+            }
+            //right
+            if (y1 ==2 ) {
+                return 0;
+            }
+            else {
+                u = temp1.array[x1][y1];
+                temp1.array[x1][y1] = temp1.array[x1][y1 + 1];
+                temp1.array[x1][y1 + 1] = u;
+            }
+            //left
+            if (y1 == 0) {
+                return 0;
+            }
+            else {
+                u = temp1.array[x1][y1];
+                temp1.array[x1][y1] = temp1.array[x1][y1 - 1];
+                temp1.array[x1][y1 - 1] = u;
+            }
+
+
+
+
+
 
 
         }
